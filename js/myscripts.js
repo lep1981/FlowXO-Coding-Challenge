97990da58fdb9299736aaad39df239b1d4863669
LUIS-PC\LUIS <info@aliasdev.com>
2018-06-18 22:00:36 +0100

    Changes for HTML and CSS
    Finished HTML structure and styling. JSON data called in via AJAX with dummy extras to test CSS on dynamically generated elements

--- js/myscripts.js
+++ js/myscripts.js
@@ -1,2 +1,15 @@
 // JavaScript Document
 
+jQuery(document).ready(function(){
+	$.ajax({
+		url: 'js/data.json',
+		dataType: 'json',
+		type: 'get',
+		cache: false,
+		success: function(data) {
+			$(data).each(function(index, value){				
+				$(".bdays-list").append('<li class="bday"><img src="'+value.photo+'" class="bday-photo" /><h3 class="bday-name">'+value.name+'</h3><p class="bday-fulldob">1 of January, 81</p><p class="bday-info">is turning <strong>37</strong> on the next <strong>1 of January</strong></p></li>');
+			});
+		}
+	});
+});
