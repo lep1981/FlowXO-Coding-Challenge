18efd86ffb27654a9d6b7d3ce07ee07895eb022e
LUIS-PC\LUIS <info@aliasdev.com>
2018-06-19 00:13:55 +0100

    Sort by name and add birthday functionality included
    I have included the sort by name code and the possibility to add new birthdays. I have excluded the possibility to add pictures as the ones shown in the initial data due to time limitations

--- js/myscripts.js
+++ js/myscripts.js
@@ -19,4 +19,60 @@ jQuery(document).ready(function(){
 			});
 		}
 	});
+	
+	
+	// Name filter
+	$.expr[':'].Contains = function(a,i,m){
+	  	return (a.textContent || a.innerText || "").toUpperCase().indexOf(m[3].toUpperCase())>=0;
+	};
+
+	function listFilter(list) {
+
+		$("#searchbox").change(function() {
+			var filter = $(this).val();
+			if(filter) {
+			  	$(list).find("h3:not(:Contains(" + filter + "))").parent().slideUp();
+			  	$(list).find("h3:Contains(" + filter + ")").parent().slideDown();
+			} else {
+			  	$(list).find("li").slideDown();
+			}
+			return false;
+		}).keyup(function() {
+			$(this).change();
+		});
+	}
+
+	$(function() {
+		listFilter($(".bdays-list"));
+	});
+	
+	
+	
+	// Modal window control
+	$("#addbdays").click(function(){
+		$("#bdays-form-wrapper").fadeIn("fast");
+	});
+	$("#close-form").click(function(){
+		$("#bdays-form-wrapper").fadeOut("fast");
+	});
+	
+	
+	// Add birthday function
+	
+	$("#submit").click(function(){
+		var fullname = $("#fullname").val();
+		var dob = $("#dob").val();
+		dob = new Date(dob);
+
+		var today = new Date();
+		var age = Math.floor((today-dob) / (365.25 * 24 * 60 * 60 * 1000) + 1);
+
+		var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		var fancydob = dob.getDate() + " of " + months[dob.getMonth()];
+		var fulldob = fancydob + ",  " + dob.getYear();
+
+		$(".bdays-list").append('<li class="bday"><img src="https://placehold.it/250x250" class="bday-photo" /><h3 class="bday-name">'+fullname+'</h3><p class="bday-fulldob">'+ fulldob +'</p><p class="bday-info">is turning <strong>'+ age +'</strong> on the next <strong>'+ fancydob +'</strong></p></li>');
+		
+		$("#bdays-form-wrapper").fadeOut("fast");
+	});
 });
