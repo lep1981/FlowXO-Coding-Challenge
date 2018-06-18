b5ed4e4ee5e40579ef91e2f6e63a08c3ea5b2fe1
LUIS-PC\LUIS <info@aliasdev.com>
2018-06-18 22:22:05 +0100

    Date calculations included
    Birthdays and ages should now be accurate

--- js/myscripts.js
+++ js/myscripts.js
@@ -7,8 +7,15 @@ jQuery(document).ready(function(){
 		type: 'get',
 		cache: false,
 		success: function(data) {
-			$(data).each(function(index, value){				
-				$(".bdays-list").append('<li class="bday"><img src="'+value.photo+'" class="bday-photo" /><h3 class="bday-name">'+value.name+'</h3><p class="bday-fulldob">1 of January, 81</p><p class="bday-info">is turning <strong>37</strong> on the next <strong>1 of January</strong></p></li>');
+			$(data).each(function(index, value){
+				var dob = new Date(value.birthday);
+				var today = new Date();
+				var age = Math.floor((today-dob) / (365.25 * 24 * 60 * 60 * 1000) + 1);
+				
+				var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+				var fancydob = dob.getDate() + " of " + months[dob.getMonth()];
+				
+				$(".bdays-list").append('<li class="bday"><img src="'+value.photo+'" class="bday-photo" /><h3 class="bday-name">'+value.name+'</h3><p class="bday-fulldob">'+ fancydob +'</p><p class="bday-info">is turning <strong>'+ age +'</strong> on the next <strong>'+ fancydob +'</strong></p></li>');
 			});
 		}
 	});
